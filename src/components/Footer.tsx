13a566c9a994e78895525525466694dc9311a270
michalzarzycki <michal.p.zarzycki@wp.pl>
2021-07-12 16:59:37 +0200

    Added navBurger

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 import styles from './Footer.module.css'
 function Footer() {
     return (
-        <div className={styles.footerContainer}>
+        <div style={{ display: 'none'}} className={styles.footerContainer}>
             FOOTER
         </div>
     )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
+import {useState} from 'react'
 import styles from './Navbar.module.css'
 function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     return (
         <nav className={styles.navbarContainer}>
             <div className={styles.mainDiv}>
@@ -8,11 +10,22 @@ function Navbar() {
                         <img className={styles.navbarImg} src="https://images.pexels.com/photos/7356/startup-photos-7356.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
                     </a>
                     <a className={styles.navTitle}>Projets</a>
-                    <div className={styles.navBurger}>
-                        <span />
-                        <span />
-                        <span />
+                    <div className={styles.navBurger} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                        <span style={{transform:  isMenuOpen ? "translateY(5px) rotate(45deg)" : ""}}/>
+                        <span style={{opacity:  isMenuOpen ? "0" : "1"}}/>
+                        <span  style={{transform:  isMenuOpen ? "translateY(-5px) rotate(-45deg)" : ""}}/>
                     </div>
+                </div>
+                <div className={styles.navMenu}   style={ { display: isMenuOpen ? 'block' : 'none' } }>
+                    <a>LINK1</a>
+                    <a>LINK2</a>
+                    <a>LINK3</a>
+                    <a>LINK1</a>
+                    <a>LINK2</a>
+                    <a>LINK3</a>
+                    <a>LINK1</a>
+                    <a>LINK2</a>
+                    <a>LINK3</a>
                 </div>
             </div>
         </nav>
