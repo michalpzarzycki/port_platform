7505766cc7b05376ceb9c7d9a163c724961f7828
michalzarzycki <michal.p.zarzycki@wp.pl>
2021-07-12 16:33:39 +0200

    Added styles to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,21 @@
 import styles from './Navbar.module.css'
 function Navbar() {
     return (
-        <div className={styles.navbarContainer}>
-            NAVBAR
-        </div>
+        <nav className={styles.navbarContainer}>
+            <div className={styles.mainDiv}>
+                <div className={styles.navbarItems}>
+                    <a className={styles.imageLink}>
+                        <img className={styles.navbarImg} src="https://images.pexels.com/photos/7356/startup-photos-7356.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+                    </a>
+                    <a className={styles.navTitle}>Projets</a>
+                    <div className={styles.navBurger}>
+                        <span />
+                        <span />
+                        <span />
+                    </div>
+                </div>
+            </div>
+        </nav>
     )
 }
 
